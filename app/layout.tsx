4433e5d40e6be17064076ec9717d8dd9f790a80f
João Paulo <joaoalencar@ioasys.com.br>
2026-01-14 11:17:54 -0300

    fixes layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -145,7 +145,7 @@ export default function RootLayout({
             <img
               height="1"
               width="1"
-              style="display:none"
+              style={{ display: "none" }}
               src="https://www.facebook.com/tr?id=2661313790928781&ev=PageView&noscript=1"
             />
           </noscript>
